f4fdc604cd63e36827b310d5b40790afa1859e22
ggao103 <78918052+ggao103@users.noreply.github.com>
2025-10-25 22:54:11 -0400

    update
    render macbook 14

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,7 +1,8 @@
-import { Box, OrbitControls } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import clsx from "clsx";
 import useMacbookStore from "../store";
+import MacbookModel14 from "./models/Macbook-14";
 
 const ProductViewer = () => {
   const {color, scale, setColor, setScale} = useMacbookStore();
@@ -36,7 +37,8 @@ const ProductViewer = () => {
             </div>
         </div>
       <Canvas id="canvas" camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far:100 }}>
-        <Box position={[0, 0, 0]} scale={10 * scale} material-color={color}></Box>
+        <ambientLight intensity={1}/>
+        <MacbookModel14 scale={0.06} position={[0, 0, 0]}></MacbookModel14>
         <OrbitControls enableZoom={false}></OrbitControls>
       </Canvas>
     </section>
